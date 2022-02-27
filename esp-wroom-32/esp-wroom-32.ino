18bdf4eaa370ffb70cb7931847359a6fa004bc65
Мартин Георгиев <martin_my@abv.bg>
2022-02-27 19:02:11 +0200

    connect LCD and show basic use

--- esp-wroom-32/esp-wroom-32.ino
+++ esp-wroom-32/esp-wroom-32.ino
@@ -1,5 +1,14 @@
 #include <ESP32Servo.h>
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
 
+// set the LCD number of columns and rows
+#define lcdColumns 16
+#define lcdRows 2
+// To get display address, run an I2C scanner sketch, default is 0x27
+LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);  
+
+// Set pin numbers for LDR voltage dividers
 #define ldr_pin_1 13
 #define ldr_pin_2 12
 // Since LDRs aren't precisely equal, different multipliers will be used to level their accuracy 
@@ -17,6 +26,18 @@ int servo_position = 0;    // Variable to store the servo
 void setup() {
   Serial.begin(9600);
 
+  // This line is required to bypass a bug with the LCD where random characters are displaied
+  // It lowers the clock from it's default value of 100 000 to lesser 10 000
+  Wire.setClock(10000);
+  // initialize LCD
+  lcd.init();
+  // turn on LCD backlight                      
+//  lcd.backlight();
+  // set cursor to first column, first row
+  lcd.setCursor(0, 0);
+  // print message
+  lcd.print("Power On :D");
+  
   // Allow allocation of all timers. This is required to controll the servos
   ESP32PWM::allocateTimer(0);
   ESP32PWM::allocateTimer(1);
@@ -50,13 +71,13 @@ void loop() {
   Serial.println( ldr2 );
 
   // Compare LDR1 and LDR2 and move the servo towards the brighter side
-  if( (ldr1 - ldr2) > (ldr1+ldr2)/2 * 0.3 ){
+  if( (ldr1 - ldr2) > (ldr1+ldr2)/2 * 0.2 ){
     if(servo_position < 180) servo_position ++;
-  }
-  if( (ldr2 - ldr1) > (ldr1+ldr2)/2 * 0.3 ){
-    if(servo_position > 0) servo_position --;
-  }
+    }
+    if( (ldr2 - ldr1) > (ldr1+ldr2)/2 * 0.2 ){
+      if(servo_position > 0) servo_position --;
+    }
+    
   myservo.write(servo_position);
-
   delay(50);
 }
