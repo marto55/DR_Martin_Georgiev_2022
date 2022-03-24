dfaf35edcd798b024c287758d6c35b9ecd3d550d
Мартин Георгиев <martin_my@abv.bg>
2022-03-24 15:18:39 +0200

    expand front end react app page

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
 
+
 function App() {
   return (
-    <div className="App">
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <iframe src="http://localhost:3000/d-solo/9X9S_DEnz/basic-graphics?orgId=1&panelId=2" width="450" height="200" frameBorder="0"></iframe>
+    <div className="App" >
+
+      <h1 id="Title">Advanced Solar Data Collector</h1>
+      <h2 id="Sub-Title">Brighter Tomorow</h2>
+
+      <div className="Rectangle">
+
+        <iframe id="PowerWeek" className="Dash-Board" src="http://localhost:3000/d-solo/9X9S_DEnz/basic-graphics?orgId=1&from=now-7d&to=now&refresh=30s&panelId=12" ></iframe>
+        <iframe className="Dash-Board" src="http://localhost:3000/d-solo/9X9S_DEnz/basic-graphics?orgId=1&from=now-24h&to=now&refresh=30s&panelId=12" ></iframe>
+        <iframe className="Dash-Board" src="http://localhost:3000/d-solo/9X9S_DEnz/basic-graphics?orgId=1&from=now-24h&to=now&refresh=30s&panelId=10"></iframe>
+        <iframe className="Dash-Board" src="http://localhost:3000/d-solo/9X9S_DEnz/basic-graphics?orgId=1&from=now-24h&to=now&refresh=30s&panelId=6"></iframe>
+        <iframe className="Dash-Board" src="http://localhost:3000/d-solo/9X9S_DEnz/basic-graphics?orgId=1&from=now-24h&to=now&refresh=30s&panelId=14"></iframe>
+ 
+      </div>
     </div>
   );
 }
